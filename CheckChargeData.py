8ea7f53241514748ff7a81de1833acd8ce160c64
mcmaponte <Matthew.Menendez-Aponte@colorado.edu>
2024-02-26 13:04:40 -0700

    Writing Code for TRR test 1.

--- CheckChargeData.py
+++ CheckChargeData.py
@@ -38,28 +38,30 @@ def CheckChargeData():
                 if len(parts) == 2:
                     label, value = parts
                     # Display label and value
-                    print(f'{label}: {value}')
+                    #print(f'{label}: {value}')
 
-                    # Convert and display battery voltage in volts if label is 'V'
+                    # Extract the values we want
                     if label == 'V':  # Battery Voltage
                         voltage_in_millivolts = float(value)
                         voltage_in_volts = voltage_in_millivolts / 1000
-                        print(f'Battery Voltage: {voltage_in_volts:.3f} V')
+                        #print(f'Battery Voltage: {voltage_in_volts:.3f} V')
 
                     if label == 'PPV': # PV Power
                         power = float(value) # W
-                        print(f'Panel Power right now: {power:.3f} W')
+                        #print(f'Panel Power right now: {power:.3f} W')
 
                     if label == 'H20':  # PV yield today
                         yield_today = float(value) # kWh
-                        print(f'Panel Yield Today: {yield_today:.3f} kWh')
+                        #print(f'Panel Yield Today: {yield_today:.3f} kWh')
 
-
-
-
-        file1 = open('ChargeLog.txt', 'a')  # open log.txt
-        file1.write('Time: ' + strftime("%Y-%m-%d %I:%M:%S %p ") + f'Voltage: {voltage_in_volts:.3f}' + f'Panel Power: {power:.3f}' + f'Yield: {yield_today: .3f}')
+        # Write values to a data file
+        file1 = open('ChargeLogVerbose.txt', 'a')  # open log.txt
+        file1.write('Time: ' + strftime("%Y-%m-%d %I:%M:%S %p ") + f'Voltage: {voltage_in_volts:.3f}' + f' Panel Power: {power:.3f}' + f' Yield: {yield_today: .3f}')
         file1.close()
+        file2 = open('ChargeLog.txt', 'a')
+        file2.write('strftime("%Y-%m-%d %I:%M:%S %p ")' +  f',{voltage_in_volts:.3f}' + f',{power:.3f}' f',{yield_today: .3f}')
+        file2.close()
+
         return(voltage_in_volts, power, yield_today)
 
     else:
