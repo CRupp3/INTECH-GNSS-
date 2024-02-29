f6cb401270e3b52c1bf8264bde1a0a584ccbb3ac
mcmaponte <Matthew.Menendez-Aponte@colorado.edu>
2024-02-29 14:00:44 -0700

    changed startuptest.py

--- startuptest.py
+++ startuptest.py
@@ -2,6 +2,10 @@
 import time
 from time import strftime
 
-for i in range(30):
+for i in range(10):
     time.sleep(1)  # sleep 1 second
     print(strftime("%S"))
+
+file1 = open("write.txt", "a")
+file1.write('hello world')
+file1.close()
