ea152edea2737518d0a8a230fa12848b99563d90
Evwi <97710583+Evwi@users.noreply.github.com>
2024-04-08 13:03:15 -0600

    Corrected returned time and added error exceptions

--- Reflectometry_Code/calculate_file_name.py
+++ Reflectometry_Code/calculate_file_name.py
@@ -3,10 +3,24 @@ from height_from_SNR import height_from_SNR
 
 def calculate_file_name(filename, QC_filename, dynamic, interpolate, printFailReasons, showAllPlots):
     height_array, height, hbar, tot_sats, time = height_from_SNR(filename, QC_filename, dynamic, interpolate, printFailReasons, showAllPlots)
-    percentage_usable_data = round(len(height_array) / tot_sats * 100, 1)
-    print(f"For the {filename}, the calculated height was {height}, with an hbar of {hbar}, with a total of {len(height_array)} ({percentage_usable_data}%) satellite arcs passing QC.")
 
+    if height_array is None:  # Check if height_array is None (indicating an error)
+        print("Error: Unable to calculate heights.")
+        return
+    
+    if tot_sats != 0:
+        percentage_usable_data = round(len(height_array) / tot_sats * 100, 1)
+    else: 
+        percentage_usable_data = 0
+    
+    print(f"For the {filename}, the calculated height was {height}, with an hbar of {hbar}, with a total of {len(height_array)} ({percentage_usable_data}%) satellite arcs passing QC.")
+    
     time = [int(item) for item in time]
-    stringtime = str(time)
-    stringtime = stringtime[1:-1]
+    # Format each component with leading zeros if necessary
+    formatted_time = [
+        str(component).zfill(2) if index != 0 else str(component)[2:]  # For the year, only take the last two digits
+        for index, component in enumerate(time)
+    ]
+    # Concatenate the formatted components into the desired string format
+    stringtime = ''.join(formatted_time)
     return height, stringtime
