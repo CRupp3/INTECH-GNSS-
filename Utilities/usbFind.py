2d2c79430d198de157788ec9bda2d77474b13145
mcmaponte <Matthew.Menendez-Aponte@colorado.edu>
2024-04-22 11:56:25 -0600

    new usb connection method

---
+++ Utilities/usbFind.py
@@ -0,0 +1,108 @@
+import re
+import subprocess
+import sys
+import usb.core
+
+def method1():
+    device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+    df = subprocess.check_output("lsusb")
+    devices = []
+    for i in df.split(b'\n'):
+        if i:
+            info = device_re.match(i)
+            if info:
+                dinfo = info.groupdict()
+                dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
+                devices.append(dinfo)
+
+    print(devices)
+
+def method2():
+    # !/usr/bin/python
+    # find USB devices
+    dev = usb.core.find(find_all=True)
+    # loop through devices, printing vendor and product ids in decimal and hex
+    for cfg in dev:
+        sys.stdout.write('Decimal VendorID=' + str(cfg.idVendor) + ' & ProductID=' + str(cfg.idProduct) + '\n')
+        sys.stdout.write('Hexadecimal VendorID=' + hex(cfg.idVendor) + ' & ProductID=' + hex(cfg.idProduct) + '\n\n')
+
+def method3():
+    # find our device
+    dev = usb.core.find(idVendor=0xfffe, idProduct=0x0001)
+
+    # was it found?
+    if dev is None:
+        raise ValueError('Device not found')
+
+    # set the active configuration. With no arguments, the first
+    # configuration will be the active one
+    dev.set_configuration()
+
+    # get an endpoint instance
+    cfg = dev.get_active_configuration()
+    intf = cfg[(0, 0)]
+
+    ep = usb.util.find_descriptor(
+        intf,
+        # match the first OUT endpoint
+        custom_match= \
+            lambda e: \
+                usb.util.endpoint_direction(e.bEndpointAddress) == \
+                usb.util.ENDPOINT_OUT)
+
+    assert ep is not None
+
+    # write the data
+    ep.write('test')
+
+def method4():
+    class find_class(object):
+        def __init__(self, class_):
+            self._class = class_
+        def __call__(self, device):
+            # first, let's check the device
+            if device.bDeviceClass == self._class:
+                return True
+            # ok, transverse all devices to find an
+            # interface that matches our class
+            for cfg in device:
+                # find_descriptor: what's it?
+                intf = usb.util.find_descriptor(
+                                            cfg,
+                                            bInterfaceClass=self._class
+                                    )
+                if intf is not None:
+                    return True
+
+            return False
+
+    printers = usb.core.find(find_all=1, custom_match=find_class(2))
+    print(printers)
+
+def method5():
+    busses = usb.busses()
+    for bus in busses:
+        devices = bus.devices
+        for dev in devices:
+            print
+            repr(dev)
+            print
+            "Device:", dev.filename
+            print
+            "  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor)
+            print
+            "  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct)
+            print
+            "Manufacturer:", dev.iManufacturer
+            print
+            "Serial:", dev.iSerialNumber
+            print
+            "Product:", dev.iProduct
+
+
+if __name__ == '__main__':
+    #method1()
+    #method2()
+    #method3()
+    #method4()
+    method5()
